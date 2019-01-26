517737b23a8a187a46125951980811b913aab865
Lucas Barros de Assis <lbassis@inf.ufrgs.br>
2019-01-26 03:30:30 +0100

    lignes disparaitent

--- Control.gd
+++ Control.gd
@@ -4,6 +4,30 @@ var _pen = null
 var _prev_mouse_pos = Vector2()
 var _line_points = Array()
 
+var drawings = Array()
+
+
+class Drawing:
+	
+	var body
+	var ttl
+	var line
+	var root
+
+	func _init(staticbody, parent, line):
+		body = staticbody
+		ttl = 150
+		root = parent
+		self.line = line
+
+	func decTime():
+		ttl = ttl-1
+		if ttl == 0:
+			root.remove_child(body)
+			root.remove_child(line)
+			return 1
+		return 0
+
 func _ready():
 	var viewport = Viewport.new()
 	var rect = get_rect()
@@ -29,8 +53,14 @@ func _ready():
 
 func _process(delta):
 	_pen.update()
+	
+	for d in drawings:
+		if d.decTime() == 1:
+			drawings.erase(d)
+	
 	if Input.is_action_just_released("click"):
 		var line = Line2D.new()
+		line.set_width(5)
 		var body = create_body(_line_points[0])
 		var length = _line_points.size()
 		
@@ -40,26 +70,30 @@ func _process(delta):
 			line.add_point(_line_points[i+1])
 			
 
-
+		var drawing = Drawing.new(body, self, line)
+		drawings.append(drawing)
+	
+		add_child(line)
 		_line_points.clear()
 		#line.set_color(Color(0,0,0))
-		line.set_width(5)
-		add_child(line)
+
 		
 
 
 func _on_draw():
 	var mouse_pos = get_local_mouse_position()
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
-		_pen.draw_line(mouse_pos, _prev_mouse_pos, Color(0, 0, 0), 3, true)
 		_line_points.append(mouse_pos)
+		draw_segment()
 
 	_prev_mouse_pos = mouse_pos
+
+func draw_segment():
+	print("ok")
 
 func create_body(pos):
 	var rigidbody = StaticBody2D.new()
 	add_child(rigidbody)
-	print("rigidbody foi")
 	return rigidbody
 
 func create_collider(start, end, body):
