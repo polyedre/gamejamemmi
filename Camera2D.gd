baea91d814fd8d24e89bd7ee468ac98f7dda1c9c
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-01-27 16:38:13 +0000

    Ajout musique et scroll auto

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,8 +1,14 @@
 extends Camera2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var is_triggered = false
+onready var player = get_parent().get_node('Player')
 
-func _ready():
-	self.make_current()
+
+func _on_Trigger_body_entered(body):
+	if body.get_name() == 'Player':
+		is_triggered = true
+		make_current()
+
+func _process(delta):
+	if is_triggered:
+		position.x += delta * 150
