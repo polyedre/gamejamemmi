a0eb9e923bf13591d036207d1727758cc633ea8f
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-01-28 08:14:35 +0000

    Ajout des fichiers qui n'ont pas été commit hier et du titre

--- Camera2D.gd
+++ Camera2D.gd
@@ -7,8 +7,12 @@ onready var player = get_parent().get_node('Player')
 func _on_Trigger_body_entered(body):
 	if body.get_name() == 'Player':
 		is_triggered = true
+		position = player.position
 		make_current()
 
 func _process(delta):
 	if is_triggered:
-		position.x += delta * 150
+		position.x += delta * 150	
+	if player.position.x + 300 < get_camera_position().x - get_viewport().get_visible_rect().size.x / 2:
+		get_tree().change_scene('res://GameOver.tscn')
+		
---
+++ README.md
@@ -0,0 +1,5 @@
+# Drawnation
+
+La vidéo de présentation peut être trouvée sur youtube à l'addresse suivante : 
+
+https://youtu.be/IBd0pH4DHAE
